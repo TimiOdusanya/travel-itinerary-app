9f574c6c06244260585bd66aab2c74a9633c4dd0
Timi Odusanya <timmycruz36@gmail.com>
2024-11-08 06:26:03 +0100

    fix: searchbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,12 +3,50 @@
 import { NavLinks, NavRight } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React,  { useState }from "react";
 import { BsChevronDown } from "react-icons/bs";
 import SearchBar from "./SearchBar";
+import { Activity, FlightData, Hotel } from "@/constants/interface";
 
 const Navbar = () => {
+  const [addedActivities, setAddedActivities] = useState<Activity[]>([]);
+  const [addedFlights, setAddedFlights] = useState<FlightData[]>([]);
+  const [addedHotels, setAddedHotels] = useState<Hotel[]>([]);
 
+
+  const [flights, setFlights] = useState<FlightData[]>([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [hotels, setHotels] = useState<Hotel[]>([]);
+
+
+  const handleFlightAddRemove = (flight: FlightData) => {
+    const flightIndex = flights.findIndex(
+      (f) => f.iataCode === flight.iataCode
+    );
+    if (flightIndex === -1) {
+      setFlights([...flights, flight]);
+    } else {
+      setFlights(flights.filter((f) => f.iataCode !== flight.iataCode));
+    }
+  };
+
+  const handleActivityAddRemove = (activity: Activity) => {
+    const activityIndex = activities.findIndex((a) => a.id === activity.id);
+    if (activityIndex === -1) {
+      setActivities([...activities, activity]);
+    } else {
+      setActivities(activities.filter((a) => a.id !== activity.id));
+    }
+  };
+
+  const handleHotelAddRemove = (hotel: Hotel) => {
+    const hotelIndex = hotels.findIndex((h) => h.hotel_id === hotel.hotel_id);
+    if (hotelIndex === -1) {
+      setHotels([...hotels, hotel]);
+    } else {
+      setHotels(hotels.filter((h) => h.hotel_id !== hotel.hotel_id)); 
+    }
+  };
 
   return (
     <nav className="navbar text-small">
@@ -17,7 +55,11 @@ const Navbar = () => {
           <Link href="/">
             <Image src="/images/logo.svg" width={54} height={54} alt="logo" />
           </Link>
-          <SearchBar />
+          <SearchBar
+            onActivityAddRemove={handleActivityAddRemove}
+            onFlightAddRemove={handleFlightAddRemove}
+            onHotelAddRemove={handleHotelAddRemove}
+          />
         </div>
 
         <ul className="xl:flex text-xs gap-5">
